372a0acae9fd0cdc34ad0e6ff65fad67ff10eb07
jjaramillod93 <jjaramillod@uni.edu.pe>
2025-02-07 12:21:50 -0500

    project 0 finished

--- project0/debug.py
+++ project0/debug.py
@@ -1,12 +1,16 @@
-def get_sum_metrics(predictions, metrics=[]):
+def get_sum_metrics(predictions, metrics=None):
+    if metrics is None:
+        metrics = []
+
     for i in range(3):
-        metrics.append(lambda x: x + i)
+        metrics.append(lambda x, val=i: x + val)
 
     sum_metrics = 0
     for metric in metrics:
         sum_metrics += metric(predictions)
 
     return sum_metrics
+
 
 
 def main():
